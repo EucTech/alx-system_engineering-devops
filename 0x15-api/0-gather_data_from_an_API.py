d8122c6a09968601aad3951a8dbf483eefeadb9a
EucTech <euccreative@gmail.com>
2023-10-24 03:29:24 +0100

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    This is a Python script that, using this REST API, for 
+    a given employee ID, returns information about his/her TODO list 
+    progress
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    employee_id = sys.argv[1]
+    
+    # get user information
+    user_info = f"{api_url}/users/{employee_id}"
+    response = requests.get(user_info)
+    user_data = response.json()
+
+    employee_name = user_data.get('name')
+
+    # Retrieve task records
+    todo_data = f"{api_url}/todos?id={employee_id}"
+    response = requests.get(todo_data)
+    todos = response.json()
+
+    complete_tasks = len(todos)
+    done_task = 0
+
+    for task in todos:
+        if task['completed']:
+            done_task += 1
+
+    print(f"Employee {employee_name} is done with tasks({done_task}/{complete_tasks}):")
+    for tsk in todos:
+        if tsk['completed']:
+            print(f"\t {tsk['title']}")
