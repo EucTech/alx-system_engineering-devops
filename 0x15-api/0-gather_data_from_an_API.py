32cb37b01ccf0009bf87b23366b5ce88d86d2e1e
EucTech <euccreative@gmail.com>
2023-10-24 03:38:36 +0100

    fixed code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-   This is a Python script that, using this REST API, for
+   This is a python script that, using this REST API, for
    a given employee ID, returns information about his/her TODO list
    progress
 """
