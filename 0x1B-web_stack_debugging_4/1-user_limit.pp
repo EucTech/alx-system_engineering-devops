152a9a3649c62c02cc728ca904b8ac08c56fcf7e
EucTech <euccreative@gmail.com>
2023-11-16 15:53:00 +0100

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,2 @@
 # Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message
 
-exec { 'comm':
-  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
-  provider => 'shell'
-}
-
-exec { 'comm':
-  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
-  provider => 'shell'
-}
